c2abde9331989131f3d78ee2120985147c74bd6f
Elżbieta Krzeczek <krzeczekelzbieta@gmail.com>
2018-12-16 11:07:29 +0100

    Update youtube.js

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+geimport axios from "axios";
 
-const KEY = "AIzaSyDJfPjJ84WzIcfwYNz57WXje6YjOLXLNK4";
-export default axios.create({
+const KEY = "";
+export default axios.cgereate({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 5,
     key: KEY
-  }
+  }ge
 });
